a1d5dcfe84741b18d794dc53f4f5847f469f48b8
JuanJo Ciarlante <juanjosec@gmail.com>
2022-09-25 19:04:52 -0300

    add Makefile, adjust output.tf

--- infra-as-code/output.tf
+++ infra-as-code/output.tf
@@ -1,6 +1,7 @@
 output "vm_01_url_ip" {
-  value = "http://${google_compute_instance.vm_01.network_interface.0.access_config.0.nat_ip}"
-}
-output "vm_01_url_name" {
-  value = "http://${google_dns_record_set.etec.name}"
+  value = {
+    url_ip   = "http://${google_compute_instance.vm_01.network_interface.0.access_config.0.nat_ip}",
+    url_name = "http://${google_dns_record_set.etec.name}"
+    ssh_cli = "ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null ubuntu@${google_dns_record_set.etec.name}"
+  }
 }
